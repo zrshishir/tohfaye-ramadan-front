d251104bbe74ac6fe8cbd29a5148a8e15a22bb16
Sagor Rana <60606778+sagorranait@users.noreply.github.com>
2024-03-06 20:05:54 +0600

    Functionality of Dua-Category is done.

--- src/screens/duas/Category.vue
+++ src/screens/duas/Category.vue
@@ -1,58 +1,74 @@
 <script>
+  import axios from 'axios';
   import { RouterLink } from 'vue-router';
   import TheHeader from '@/components/TheHeader.vue';
+  import TheLoading from '@/components/TheLoading.vue';
+  import TheError from '@/components/TheError.vue';
 
   export default {
     components: { 
-      TheHeader 
+      TheHeader,
+      TheLoading,
+      TheError
+    },
+    data(){
+      return {
+        error: false,
+        loading: false,
+        duaCategory: [],
+        storedDuaCategory: localStorage.getItem('Dua-Category'),
+      }
+    },
+    methods: {
+      async fetchData() {
+        this.loading = true;
+
+        if (this.storedDuaCategory) {
+          this.duaCategory = JSON.parse(this.storedDuaCategory);
+          this.loading = false;
+          this.error = false;
+        } else {
+          try {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/doa-category`);
+            this.duaCategory = response.data?.data;
+            localStorage.setItem('Dua-Category', JSON.stringify(response.data?.data));
+            this.loading = false;
+            this.error = false;           
+          } catch (error) {
+            this.loading = false;
+            this.error = true;
+            console.error('Error fetching data:', error);
+          }
+        }
+      }
+    },
+    mounted() {
+      this.fetchData();
     }
   }
 </script>
 
 <template>
-  <TheHeader title="Dua Category"/>
-  <div class="tasbih-area px-5 pb-3">
-    <div class="surahs mt-5">
-      <RouterLink :to="`/duas/category-duas/${1}`" class="surah block border border-primary rounded-xl p-3 my-3">
-        <div class="content flex items-center gap-3">          
-          <div class="number w-10 h-10 bg-union bg-no-repeat bg-center bg-cover flex items-center justify-center">
-            <span class="text-white font-bold">01</span>
-          </div>
-          <div class="text-en">
-            <p class="text-lg font-bold">Salat</p>
-          </div>
-        </div>
-      </RouterLink>
-      <RouterLink :to="`/duas/category-duas/${2}`" class="surah block border border-primary rounded-xl p-3 my-3">
-        <div class="content flex items-center gap-3">          
-          <div class="number w-10 h-10 bg-union bg-no-repeat bg-center bg-cover flex items-center justify-center">
-            <span class="text-white font-bold">02</span>
-          </div>
-          <div class="text-en">
-            <p class="text-lg font-bold">Siam</p>
-          </div>
-        </div>
-      </RouterLink>
-      <RouterLink :to="`/duas/category-duas/${3}`" class="surah block border border-primary rounded-xl p-3 my-3">
-        <div class="content flex items-center gap-3">          
-          <div class="number w-10 h-10 bg-union bg-no-repeat bg-center bg-cover flex items-center justify-center">
-            <span class="text-white font-bold">03</span>
-          </div>
-          <div class="text-en">
-            <p class="text-lg font-bold">Zikir</p>
-          </div>
-        </div>
-      </RouterLink>
-      <RouterLink :to="`/duas/category-duas/${4}`" class="surah block border border-primary rounded-xl p-3 my-3">
-        <div class="content flex items-center gap-3">          
-          <div class="number w-10 h-10 bg-union bg-no-repeat bg-center bg-cover flex items-center justify-center">
-            <span class="text-white font-bold">04</span>
-          </div>
-          <div class="text-en">
-            <p class="text-lg font-bold">Every Day</p>
-          </div>
-        </div>
-      </RouterLink>
+  <the-loading v-if="loading">
+    <div class="page-title text-center">
+      <img class="w-20 m-auto" src="@/assets/images/icons/dua.svg" alt="Dua-Category">
+      <h1 class="text-2xl font-bold mt-3">Dua Category</h1>
     </div>
-  </div>
+  </the-loading>
+  <TheError v-if="error"/>
+  <template v-if="!loading"> 
+    <TheHeader title="Dua Category"/>
+    <div class="tasbih-area px-5 pb-3">
+      <div class="surahs mt-5 grid grid-cols-2 gap-4">
+        <RouterLink v-for="(data, index) in duaCategory" :key="index" :to="`/duas/category-duas/${data?.id}`" class="surah block border border-primary rounded-xl p-3">
+          <div class="content flex items-center justify-center gap-3">
+            <div class="text-en text-center">
+              <p class="text-lg font-bold pb-1">{{ data?.name }}</p>
+              <p class="text-base">{{data?.bangla_text}} ({{ data?.arabic_text }})</p>
+            </div>
+          </div>
+        </RouterLink>
+      </div>
+    </div>
+  </template>
 </template>
