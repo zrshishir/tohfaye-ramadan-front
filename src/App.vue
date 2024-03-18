ba055ab4a40765cd751196c50cb12d272f3af890
Sagor Rana <60606778+sagorranait@users.noreply.github.com>
2024-03-18 19:20:34 +0600

    Fixed the Phone route problem.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script>
+  import { App as CapacitorApp } from '@capacitor/app';
   export default {
-
+    mounted() {
+      CapacitorApp.addListener('backButton', () => {
+        this.handleBackButton();
+      });
+    },
+    methods: {
+      handleBackButton() {
+        if (this.$route.path !== '/') {
+          this.$router.go(-1);
+        } else {
+          CapacitorApp.exitApp();
+        }
+      }
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './routes/index';
 
 const app = createApp(App);
