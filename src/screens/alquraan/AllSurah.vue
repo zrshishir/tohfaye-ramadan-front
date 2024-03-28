8af78896abd81981158836391a4acda8109f3f76
Sagor Rana <60606778+sagorranait@users.noreply.github.com>
2024-03-28 11:27:16 +0600

    Solved the Filter Duas and Surahs.

--- src/screens/alquraan/AllSurah.vue
+++ src/screens/alquraan/AllSurah.vue
@@ -17,7 +17,7 @@
         show: false,
         loading: false,
         isModalOpen: false,
-        selectedSurah: 'all',
+        selectedOption: 'Select Your Surah',
         storedSurah: localStorage.getItem('surah'),
       }
     },
@@ -47,12 +47,14 @@
       handleSelectSurah() {
         this.surahs = JSON.parse(this.storedSurah);
 
-        if (this.selectedSurah === 'all') {
+        if (this.selectedOption === 'Select Your Surah') {
           this.surahs = JSON.parse(this.storedSurah);
         } else {
           // Filter the surahs to display only the selected surah
-          this.surahs = JSON.parse(this.storedSurah).filter(surah => surah.id === this.selectedSurah);
+          this.surahs = JSON.parse(this.storedSurah).filter(surah => surah.name === this.selectedOption);
         }
+
+        this.isModalOpen = false;
       },
       openModal() {
         this.isModalOpen = true;
@@ -81,17 +83,45 @@
     <TheNoData v-if="surahs === 0"/>
     <div v-if="surahs !== 0" class="tasbih-area px-5 pb-3">
       <div v-if="show" class="relative">
-        <select v-model="selectedSurah" @change="handleSelectSurah" class="block appearance-none w-full bg-white border border-primary px-4 py-2 pr-8 rounded-xl focus:outline-none focus:shadow-outline">
-          <option value="all">Select Your Surah</option>
-          <option v-for="(surah, index) in JSON.parse(this.storedSurah) || surahs" :key="index" :value="surah?.id">{{ surah?.name }} ({{ surah?.arabic_name }})</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M10 12.586l4.293-4.293 1.414 1.414L10 15.414l-5.707-5.707 1.414-1.414z"/>
-          </svg>
+        <button @click="openModal" class="w-full bg-white border border-primary px-4 py-3 pr-8 rounded-xl focus:outline-none focus:shadow-outline">
+          <span class="w-full block text-left text-base font-medium">{{ selectedOption }}</span>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M10 12.586l4.293-4.293 1.414 1.414L10 15.414l-5.707-5.707 1.414-1.414z"/>
+            </svg>
+          </div>
+        </button>
+        <!-- Modal Start -->
+        <div v-if="isModalOpen" class="fixed inset-0 flex items-center justify-center z-10">
+          <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+
+          <div class="modal-container bg-white w-11/12 h-5/6 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+            <!-- Modal content -->
+            <div class="modal-content h-full py-4 text-left px-6 flex flex-col">
+              <!-- Modal header -->
+              <div class="modal-header pb-3">
+                <p class="text-xl text-center font-bold">Select Your Surah</p>
+              </div>
+              <!-- Modal body -->
+              <div class="modal-body flex-grow overflow-y-auto pb-3">
+                <!-- Modal content goes here -->
+                <label class="flex items-center justify-between bg-white border border-primary px-4 py-3 mr-3 mb-3 rounded">
+                  <span>All Surah</span>
+                  <input type="radio" v-model="selectedOption" @change="handleSelectSurah" value="Select Your Surah" class="mr-2">
+                </label>
+                <label v-for="(surah, index) in JSON.parse(this.storedSurah) || surahs" :key="index" class="flex items-center justify-between bg-white border border-primary px-4 py-3 mr-3 mb-3 rounded">
+                  <span>{{ surah?.name }}</span>
+                  <input type="radio" v-model="selectedOption" @change="handleSelectSurah" :value="surah?.name" class="mr-2">
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
+        <!-- Modal End -->
       </div>
       <div class="surahs mt-3">
+        {{ console.log(selectedOption)
+         }}
         <RouterLink 
           v-for="(surah, index) in surahs" 
           :key="index" 
